3de560161eb72a11266ff21d3af18c34616e0ea7
jeffreyai7 <jirukeh@gmail.com>
2025-02-10 04:55:53 +0100

    complete the user service calls

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from "typeorm";
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "./user.entity";
+
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    constructor(@InjectRepository(User) private usersRepository: Repository<User>) {}
+
+        create(email: string, password: string) {
+        const user = this.usersRepository.create({ email, password });
+         
+        return this.usersRepository.save(user);
+        }
+
+        find(email: string) {
+        return this.usersRepository.find({ where: { email } });
+        }
+
+    
+        findOne(id: number){
+        return this.usersRepository.findOne({where: {id}});
+        }
+
+        async update(id: number, attrs: Partial<User>) {
+            const user = await this.findOne(id);
+            if(!user){
+                throw new Error('User not found');
+            }
+            Object.assign(user, attrs);
+            return this.usersRepository.save(user);
+        }
+
+        async remove(id: number) {
+            const user = await this.findOne(id);
+            if(!user){
+                throw new Error('User not found');
+            }
+            return this.usersRepository.remove(user);
+
+        }
+
+    }
+
+const usersService = new UsersService( {} as any);    
+
+

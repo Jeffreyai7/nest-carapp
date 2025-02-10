dd0975cdd521bd6a3f02e28b91594649433e43fe
jeffreyai7 <jirukeh@gmail.com>
2025-02-10 06:07:18 +0100

    implement delete functionality

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CreateUserDto } from './create.user.dto';
+import { Body, Controller, Post, Get, Patch, Param, Query, Delete } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create.user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -14,5 +14,19 @@ export class UsersController {
 
     }
 
+    @Get("/user")
+    findAllUsers(@Query("email") email: string){
+        return this.usersService.find(email);
+    }
+    
+    @Get("/:id")
+    findUser(@Param("id") id:string){
+        return this.usersService.findOne(parseInt(id));
+    }
+
+    @Delete("/:id")
+    removeUser(@Param("id") id: string){
+        return this.usersService.remove(parseInt(id));
+    }
 
 }

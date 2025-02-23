31b435369d2f7d33daf0c02d533bd67790da82c8
jeffreyai7 <jirukeh@gmail.com>
2025-02-23 04:48:57 +0100

    add types to dto

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -3,12 +3,13 @@ import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import { plainToInstance } from "class-transformer";
 
-
-
-export function Serialize(dto: any){
-    return UseInterceptors(new SerializeInterceptor(dto));
+interface ClassConstructor {
+    new (...args: any[]): {}    
 }
 
+export function Serialize(dto: ClassConstructor){
+    return UseInterceptors(new SerializeInterceptor(dto));
+}
 
 export class SerializeInterceptor implements NestInterceptor{
     constructor(private dto: any) {}

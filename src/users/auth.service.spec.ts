88c2b0b16a209a2f493f07f9a959b0583da1c34d
jeffreyai7 <jirukeh@gmail.com>
2025-03-05 04:42:40 +0100

    add type safety to auth test

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,14 +1,15 @@
 import { Test } from "@nestjs/testing";
 import { AuthService } from "./auth.service";
 import { UsersService } from "./users.service"; 
+import { User } from "./user.entity";
 
 
 
 it("cratea an instance of AuthService", async () => {
     
-    const fakeUsersService = {
+    const fakeUsersService : Partial<UsersService> = {
         find: () => Promise.resolve([]),
-        create: (email: string, password: string) => Promise.resolve({id: 1, email, password})
+        create: (email: string, password: string) => Promise.resolve({id: 1, email, password} as User)
     }    
     
     const module = await Test.createTestingModule({

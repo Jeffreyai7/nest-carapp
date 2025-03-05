4d045db10e6f562c798a46e019afde6798eee17d
jeffreyai7 <jirukeh@gmail.com>
2025-03-05 05:40:55 +0100

    add error on signup test

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,13 +2,16 @@ import { Test } from "@nestjs/testing";
 import { AuthService } from "./auth.service";
 import { UsersService } from "./users.service"; 
 import { User } from "./user.entity";
+import { BadRequestException } from "@nestjs/common";
 
 
 describe("AuthService", () => {
     let service: AuthService;
 
+    let fakeUsersService: Partial<UsersService>;
+    
     beforeEach(async () => {
-        const fakeUsersService : Partial<UsersService> = {
+        fakeUsersService = {
             find: () => Promise.resolve([]),
             create: (email: string, password: string) => Promise.resolve({id: 1, email, password} as User)
         }    
@@ -39,4 +42,13 @@ describe("AuthService", () => {
         expect(hash).toBeDefined();
     })
 
+
+    it('throws an error if user signs up with email that is in use', async () => {
+        fakeUsersService.find = () =>
+     
+          Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+        await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
+          BadRequestException,
+        );
+      });
 })

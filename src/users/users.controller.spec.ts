28f5a9a889d9ce9698b8ebd558bd16e4716552a8
jeffreyai7 <jirukeh@gmail.com>
2025-03-07 14:45:26 +0100

    implement login test for conmtroller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -24,7 +24,9 @@ describe('UsersController', () => {
 
     fakeAuthService = {
     //  signup: () => {},
-    //   signin: () => {}
+      signin: (email:string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User);
+      }
     };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
@@ -63,5 +65,16 @@ describe('UsersController', () => {
     await expect(controller.findUser('1')).rejects.toThrow(NotFoundException);
   });
 
-});
 
+  it("signin updates session object and returns user", async() => {
+    const session = { userId: -10 };
+    const user = await controller.login({email: "asdf@asdf.com", password: "asdf" }, session);
+
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
+
+  });
+
+
+
+});

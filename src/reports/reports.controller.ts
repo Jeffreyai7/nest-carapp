c646f8b2ae6b3284747ec68c63ff7982a215c686
jeffreyai7 <jirukeh@gmail.com>
2025-03-14 06:07:36 +0100

    add serializer to the reports controller

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -4,6 +4,8 @@ import { ReportsService } from './reports.service';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { CurrentUser } from 'src/users/decorators/current-user.decorator';
 import { User } from 'src/users/user.entity';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { ReportDto } from './dtos/report.dto';
 
 
 @Controller('reports')
@@ -12,7 +14,10 @@ constructor(private reportsService: ReportsService) {}
 
 @Post()
 @UseGuards(AuthGuard)
+@Serialize(ReportDto)
     createReport(@Body() body: CreateReportDto, @CurrentUser() user: User ) {
         return this.reportsService.create(body, user);
     }
 }
+
+

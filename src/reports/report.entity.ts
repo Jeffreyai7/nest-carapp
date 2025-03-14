aebe142d2de03837175e4f92ed3f5b35733b409f
jeffreyai7 <jirukeh@gmail.com>
2025-03-14 04:46:41 +0100

    implement associations to typeorm user and report entity

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "src/users/user.entity";
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from "typeorm";
 
 @Entity()
 export class Report{
@@ -25,6 +26,10 @@ export class Report{
 
     @Column()
     mileage: number;
+
+    @ManyToOne(() => User, (user) => user.reports)
+    user:User;
+
 
     
 }

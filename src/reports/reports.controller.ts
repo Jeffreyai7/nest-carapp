1e5647117a4b8fe13893590fc9e0da9ea8607150
jeffreyai7 <jirukeh@gmail.com>
2025-03-20 01:28:49 +0100

    implement patch request handler

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from 'src/guards/auth.guard';
@@ -6,7 +6,7 @@ import { CurrentUser } from 'src/users/decorators/current-user.decorator';
 import { User } from 'src/users/user.entity';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
-
+import { ApproveReportDto } from './dtos/approve-report.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -18,6 +18,12 @@ constructor(private reportsService: ReportsService) {}
     createReport(@Body() body: CreateReportDto, @CurrentUser() user: User ) {
         return this.reportsService.create(body, user);
     }
+
+    @Patch("/:id")
+    approvedReport(@Param("id") id: string, @Body() body: ApproveReportDto){
+        
+    }
+
 }
 
 

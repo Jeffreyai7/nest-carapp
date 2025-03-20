cf82e2da0bc5e97ee462635e3cf2527bb30caf98
jeffreyai7 <jirukeh@gmail.com>
2025-03-20 03:47:31 +0100

    implement middleware function for authorization

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -7,6 +7,7 @@ import { User } from 'src/users/user.entity';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
 import { ApproveReportDto } from './dtos/approve-report.dto';
+import { AdminGuard } from 'src/guards/admin.guard';
 
 @Controller('reports')
 export class ReportsController {
@@ -20,6 +21,7 @@ constructor(private reportsService: ReportsService) {}
     }
 
     @Patch("/:id")
+    @UseGuards(AdminGuard)
     approvedReport(@Param("id") id: string, @Body() body: ApproveReportDto){
         return this.reportsService.changeApproval(id, body.approved)
     }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,20 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './user.entity';
 import { AuthService } from './auth.service';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { CurrentUserMiddleware } from './middlewares/current-user.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   providers: [UsersService,
-     AuthService, {
-      provide: 'APP_INTERCEPTOR', 
-      useClass: CurrentUserInterceptor
-     }
+     AuthService
      ],
   controllers: [UsersController]
 })
-export class UsersModule {}
+export class UsersModule {
+  configure(consumer: MiddlewareConsumer){
+    consumer.apply(CurrentUserMiddleware).forRoutes('*');
+  }
+}
